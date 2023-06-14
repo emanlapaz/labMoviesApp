b79a753155dc693479b79dba3599a83d946e20bf
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-06-14 07:25:45 +0100

    Added routing and live data from API.

--- src/pages/movieDetailsPage.jsx
+++ src/pages/movieDetailsPage.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState, useEffect}  from "react";
+import { useParams } from "react-router-dom";
 import MovieHeader from "../components/headerMovie/";
 import MovieDetails from "../components/movieDetails/";
 import Grid from "@mui/material/Grid";
@@ -12,11 +13,36 @@ const styles = {
     justifyContent: "space-around",
   },
 };
-
 const MoviePage = (props) => {
-  const movie = props.movie;
-  const images = props.images;
-
+    const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+    const [images, setImages] = useState([]);
+  
+    useEffect(() => {
+      fetch(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((movie) => {
+          // console.log(movie)
+          setMovie(movie);
+        });
+    }, [id]);
+  
+    useEffect(() => {
+      fetch(
+        `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
+      )
+        .then((res) => res.json())
+        .then((json) => json.posters)
+        .then((images) => {
+          // console,log(images)
+          setImages(images);
+        });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
   return (
     <>
       {movie ? (

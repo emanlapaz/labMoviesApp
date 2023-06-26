72efba6d96648a508e1f26704a7fc91d3c95b362
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-06-26 02:50:51 +0100

    Tagging upcoming movies as must-watch.

--- src/contexts/moviesContext.jsx
+++ src/contexts/moviesContext.jsx
@@ -3,15 +3,18 @@ import React, { useState, useEffect } from "react";
 export const MoviesContext = React.createContext(null);
 
 const MoviesContextProvider = (props) => {
+  // State variables
   const [myReviews, setMyReviews] = useState({});
   const [favourites, setFavourites] = useState([]);
   const [playlists, setPlaylists] = useState([]);
 
+  // Side effect for logging favourites and playlists
   useEffect(() => {
     console.log("Favourites:", favourites);
-    console.log("Must Watch:", playlists);
+    console.log("Playlists:", playlists);
   }, [favourites, playlists]);
 
+  // Function to add a movie to favourites
   const addToFavourites = (movie) => {
     let updatedFavourites = [...favourites];
     if (!favourites.includes(movie.id)) {
@@ -20,6 +23,7 @@ const MoviesContextProvider = (props) => {
     setFavourites(updatedFavourites);
   };
 
+  // Function to add a movie to playlists
   const addToPlaylists = (movie) => {
     let updatedPlaylists = [...playlists];
     if (!playlists.includes(movie.id)) {
@@ -28,14 +32,17 @@ const MoviesContextProvider = (props) => {
     setPlaylists(updatedPlaylists);
   };
 
+  // Function to remove a movie from favourites
   const removeFromFavourites = (movie) => {
     setFavourites(favourites.filter((mId) => mId !== movie.id));
   };
 
+  // Function to add a review for a movie
   const addReview = (movie, review) => {
     setMyReviews({ ...myReviews, [movie.id]: review });
   };
 
+  // Provide the context value to the child components
   return (
     <MoviesContext.Provider
       value={{
